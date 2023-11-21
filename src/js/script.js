bfa18c0835771b8da2f535407321bc4562d3d8c7
Bruno Galvao Martins <bruno-g-martins@outlook.com>
2023-11-21 19:25:02 -0300

    added js functions to page

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,37 @@
 import MenuMobile from "./modules/menu-mobile.js";
+import Typewriter from "./modules/typewriter.js";
+import SmoothScroll from "./modules/smoothScroll.js";
+import AnimaScroll from "./modules/animaScroll.js";
+import Accordion from "./modules/accordion.js";
 
 const mobileMenu = new MenuMobile(
   '[data-mobile="button"]',
   '[data-mobile="list"]'
 );
 mobileMenu.init();
-console.log("funcionou");
+
+const links = new SmoothScroll('a[href^="#"]');
+links.init();
+
+function timewriter() {
+  const textwriter = new Typewriter(".phrase");
+  textwriter.init();
+}
+setInterval(timewriter, 3000);
+
+const scrollAnima = new AnimaScroll('[data-anima="scroll"]');
+scrollAnima.init();
+
+const accordion = new Accordion(".contact-dt");
+accordion.init();
+
+window.addEventListener("scroll", () => {
+  const navbar = document.getElementById("navbar");
+  if (window.scrollY > 500) {
+    navbar.style.position = "fixed";
+    navbar.style.top = 0;
+    navbar.style.zIndex = 9999;
+  } else {
+    navbar.style.position = "relative";
+  }
+});
